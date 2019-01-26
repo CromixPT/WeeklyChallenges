4f133f897ed9cbd35f4512caae5482e186be3b80
CromixPT <tiago.g.candeias@gmail.com>
2019-01-26 05:44:51 +0000

    Text Files in C# challenge first version
    Managed to read the list to the form window, also made the add button work. Still need to finish the save list funcionallity

--- TextFileChallenge/TextFileChallenge/ChallengeForm.cs
+++ TextFileChallenge/TextFileChallenge/ChallengeForm.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TextFileChallenge
 {
-    public partial class ChallengeForm : Form
+    public partial class ChallengeForm:Form
     {
         BindingList<UserModel> users = new BindingList<UserModel>();
 
@@ -25,6 +21,50 @@ namespace TextFileChallenge
         {
             usersListBox.DataSource = users;
             usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            var newUser = new UserModel();
+
+            newUser.FirstName = firstNameText.Text;
+            newUser.LastName = lastNameText.Text;
+            newUser.Age = (int)agePicker.Value;
+            if(!isAliveCheckbox.Checked)
+            {
+                newUser.IsAlive = false;
+            }
+            else
+            {
+                newUser.IsAlive = true;
+            }
+
+
+
+            users.Add(newUser);
+        }
+
+        private void ChallengeForm_Load(object sender, EventArgs e)
+        {
+            string filePath = @"D:\Repos\WeeklyChallenges\TextFileChallenge\TextFileChallenge\StandardDataSet.csv";
+
+            var header = File.ReadLines(filePath);
+            var lines = File.ReadAllLines(filePath).Skip(1).ToList();
+
+            foreach(string line in lines)
+            {
+                string[] info = line.Split(',');
+
+                var newUser = new UserModel();
+
+                newUser.FirstName = info[0];
+                newUser.LastName = info[1];
+                newUser.Age = Convert.ToInt32(info[2]);
+
+                newUser.IsAlive = Convert.ToBoolean(Convert.ToInt32(info[3]));
+
+                users.Add(newUser);
+            }
         }
     }
 }
